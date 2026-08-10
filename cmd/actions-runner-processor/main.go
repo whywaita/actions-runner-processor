331d49b72e9eb4e0c2fbc6938d1fed00c25b100d
whywaita <whywaita@whywrite.it>
2026-08-10 13:17:27 +0900

    fix errcheck: check f.Close return value

--- cmd/actions-runner-processor/main.go
+++ cmd/actions-runner-processor/main.go
@@ -244,7 +244,7 @@ func checkFuseConf() error {
 	if err != nil {
 		return fmt.Errorf("cannot open /etc/fuse.conf: %w", err)
 	}
-	defer f.Close()
+	defer func() { _ = f.Close() }()
 
 	scanner := bufio.NewScanner(f)
 	for scanner.Scan() {
